cde143ccf093ae9984736f7d24d395cee1d8e1f3
Orkhan Mustafayev <orkhanmustafayev44@gmail.com>
2024-01-12 11:39:10 +0400

    Task12

---
+++ Task12.py
@@ -0,0 +1,19 @@
+import redis
+import networkx as nx
+import matplotlib.pyplot as plt
+
+redis_client = redis.StrictRedis(host='91.102.161.166', port=6379, db=0)
+
+redis_client.set('OrkhanMustafayev', '09-11-2001')
+
+G = nx.Graph()
+
+G.add_node("Orkhan")
+G.add_node("Ali")
+G.add_node("Farid")
+
+G.add_edge("Orkhan", "Farid")
+G.add_edge("Farid", "Ali")
+
+nx.draw(G, with_labels=True)
+plt.show()
